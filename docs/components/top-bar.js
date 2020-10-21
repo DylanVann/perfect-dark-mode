dd63711f6e2c110a1f53efba85bef2a609c7118b
Dylan Vann <dylan@dylanvann.com>
2020-10-21 14:46:32 -0400

    docs: fix top bar pointer events

--- docs/components/top-bar.js
+++ docs/components/top-bar.js
@@ -10,13 +10,19 @@ window.addEventListener(
     if (window.scrollY > 200) {
       if (!showingFloatingButton) {
         floatingButtonEl.classList.replace('opacity-0', 'opacity-100')
-        topBarEl.classList.replace('pointer-events-none', 'pointer-events-auto')
+        floatingButtonEl.classList.replace(
+          'pointer-events-none',
+          'pointer-events-auto',
+        )
         showingFloatingButton = true
       }
     } else {
       if (showingFloatingButton) {
         floatingButtonEl.classList.replace('opacity-100', 'opacity-0')
-        topBarEl.classList.replace('pointer-events-auto', 'pointer-events-none')
+        floatingButtonEl.classList.replace(
+          'pointer-events-auto',
+          'pointer-events-none',
+        )
         showingFloatingButton = false
       }
     }
