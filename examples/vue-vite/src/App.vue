f8b1ec7f06435a2e272da0d2927aac514588160a
Dylan Vann <dylan@dylanvann.com>
2020-10-25 01:12:25 -0400

    docs: finish vue example

--- examples/vue-vite/src/App.vue
+++ examples/vue-vite/src/App.vue
@@ -1,15 +1,19 @@
 <template>
+  <Toggle />
+  <br />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
+import Toggle from './components/Toggle.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Toggle,
+    HelloWorld,
+  },
 }
 </script>
