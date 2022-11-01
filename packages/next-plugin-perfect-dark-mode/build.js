60761ea789b0888b0ceedf0c9943a3544ca5a95e
Dylan Vann <dylan@dylanvann.com>
2022-11-01 00:40:59 -0400

    feature: version 2
    BREAKING CHANGE

--- packages/next-plugin-perfect-dark-mode/build.js
+++ packages/next-plugin-perfect-dark-mode/build.js
@@ -1,17 +1,18 @@
-import execa from 'execa'
+import { execa } from 'execa'
 
 const run = async () => {
-  await execa('esbuild', [
-    '--bundle',
-    'src/index.ts',
-    '--platform=browser',
+  await execa(`esbuild`, [
+    `--bundle`,
+    `src/index.tsx`,
+    `--platform=browser`,
     `--outfile=dist/index.js`,
     `--format=esm`,
-    '--external:react',
-    '--external:perfect-dark-mode',
-    '--external:react-perfect-dark-mode',
-    '--external:next',
-    '--minify',
+    `--jsx=automatic`,
+    `--external:react`,
+    `--external:perfect-dark-mode`,
+    `--external:react-perfect-dark-mode`,
+    `--external:next`,
+    `--minify`,
   ])
 }
 
